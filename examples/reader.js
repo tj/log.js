82ddc0776cb67005f62bf0255558287fc662d5d8
Tj Holowaychuk <tj@vision-media.ca>
2010-09-25 18:39:26 -0700

    Started reader

---
+++ examples/reader.js
@@ -0,0 +1,13 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Log = require('../lib/log')
+  , fs = require('fs')
+  , stream = fs.createReadStream(__dirname + '/file.log')
+  , log = new Log('debug', stream);
+
+log.on('line', function(line){
+  console.log(line);
+});;
