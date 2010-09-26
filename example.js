d4ce7a8b788e1e3885d80d50b5d3031efd3fb308
Tj Holowaychuk <tj@vision-media.ca>
2010-09-25 18:02:27 -0700

    More examples

--- example.js
+++ example.js
@@ -6,6 +6,11 @@
 var Log = require('./lib/log')
   , log = new Log(Log.INFO);
 
-log.debug('preparing email');
-log.info('sending email');
-log.error('failed to send email');
+log.debug('a debug message');
+log.info('a info message');
+log.notice('a notice message');
+log.warning('a warning message');
+log.error('a error message');
+log.critical('a critical message');
+log.alert('a alert message');
+log.emergency('a emergency message');
